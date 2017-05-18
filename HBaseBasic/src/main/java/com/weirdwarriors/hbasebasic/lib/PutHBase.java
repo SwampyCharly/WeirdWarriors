38a0f5ca36b644565e0a872eab6f6fbb261d357b
SwampyCharly <carloentre@gmail.com>
2017-05-18 17:58:21 +0200

    Add files via upload

--- HBaseBasic/src/main/java/com/weirdwarriors/hbasebasic/lib/PutHBase.java
+++ HBaseBasic/src/main/java/com/weirdwarriors/hbasebasic/lib/PutHBase.java
@@ -6,17 +6,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.HColumnDescriptor;
-import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Table;
+import org.apache.log4j.Logger;
 
 public class PutHBase 
 {	
+	private static Logger logger = Logger.getLogger(PutHBase.class);
 	private String hostName;
 	private Connection connection;
 	
@@ -29,29 +28,14 @@ public class PutHBase
         conf.set("hbase.zookeeper.property.clientPort", zookeeperPort);
         conf.set("hbase.zookeeper.quorum", hostName);
         connection = ConnectionFactory.createConnection(conf);
+        
+        logger.info("HBase configuration created and PutHBase instanced.");
 	}
 	
-	public Table getTable(String namespaceString, String tableString, List<String> columnDescriptors) throws IOException
+	public Table getTable(String namespaceString, String tableString) throws IOException
 	{
 		// For table name with name space
         String tableName = namespaceString + ":" + tableString;
-        
-        // Get table descriptor
-        Admin admin = connection.getAdmin();
-        HTableDescriptor tabledescriptor = new HTableDescriptor(TableName.valueOf(tableName));
-
-        // Add column families
-        for (int i = 0; i < columnDescriptors.size(); i++)
-        {
-	        HColumnDescriptor familyData = new HColumnDescriptor(columnDescriptors.get(i).getBytes());
-	        tabledescriptor.addFamily(familyData);
-        }
-
-        // If table does not exist, then create it
-        if (!admin.tableExists(TableName.valueOf(tableName))) 
-        {
-            admin.createTable(tabledescriptor);
-        }
         
         // Get the table
         return connection.getTable(TableName.valueOf(tableName));
@@ -59,15 +43,13 @@ public class PutHBase
 	
     public void put(String namespaceString, String tableString, List<String> columnDescriptors, List<PutValue> putValues) throws Exception, IOException 
     {    	
-    	Table table = getTable(namespaceString, tableString, columnDescriptors);
+    	Table table = getTable(namespaceString, tableString);
         
         // List with puts
         List<Put> putList = new ArrayList<Put>();
         PutValue pv;
         // List with row keys, column families, columns and values
         List<ColumnValue> columnValues;
-        
-        String columnFamilyString;
         
         for (int i = 0; i < putValues.size(); i++)
         {
@@ -82,15 +64,8 @@ public class PutHBase
         	
         	// Add column to the put and add put to the put list
         	for (int j = 0; j < columnValues.size(); j++)
-        	{
-        		columnFamilyString = columnValues.get(j).getColumnFamily();
-        		if (!columnDescriptors.contains(columnFamilyString))
-        		{
-        			// TOREVIEW: Is this necessary?
-        			throw new Exception("Incorrect column family in the put operation.");
-        		}
-        		
-        		put.addColumn(columnFamilyString.getBytes(), columnValues.get(j).getQualifier().getBytes(), columnValues.get(j).getValue().getBytes());
+        	{        		
+        		put.addColumn(columnValues.get(j).getColumnFamily().getBytes(), columnValues.get(j).getQualifier().getBytes(), columnValues.get(j).getValue().getBytes());
         		
         		putList.add(put);
         	}
@@ -102,37 +77,9 @@ public class PutHBase
     
     public void put(String namespaceString, String tableString, List<String> columnDescriptors, PutValue putValue) throws Exception, IOException 
     {    	
-    	Table table = getTable(namespaceString, tableString, columnDescriptors);
-        
-        // List with puts
-        List<Put> putList = new ArrayList<Put>();
-        // List with row keys, column families, columns and values
-        List<ColumnValue> columnValues;
-        	
-        // Create instance of put
-    	Put put = new Put(String.valueOf(putValue.getRowKey()).getBytes());
+    	List<PutValue> putList = new ArrayList<PutValue>();
+    	putList.add(putValue);
     	
-    	// Get column values for this put
-    	columnValues = putValue.getColumnValues();
-        
-        String columnFamilyString;
-    	
-    	// Add column to the put and add put to the put list
-    	for (int j = 0; j < columnValues.size(); j++)
-    	{
-    		columnFamilyString = columnValues.get(j).getColumnFamily();
-    		if (!columnDescriptors.contains(columnFamilyString))
-    		{
-    			// TOREVIEW: Is this necessary?
-    			throw new Exception("Incorrect column family in the put operation.");
-    		}
-    		
-    		put.addColumn(columnValues.get(j).getColumnFamily().getBytes(), columnValues.get(j).getQualifier().getBytes(), columnValues.get(j).getValue().getBytes());
-    		
-    		putList.add(put);
-    	}
-        
-    	// Put operation
-        table.put(putList);
+    	put(namespaceString, tableString, columnDescriptors, putList);
     }
 }
